8e4abc47eba88f5f4bc7b5b62cc55e22b64a0b5d
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-10-13 20:11:31 +0200

    Simple test program to create library

---
+++ clang_gcc/print.cxx
@@ -0,0 +1,8 @@
+#include <string>
+#include <iostream>
+
+void print_message(const std::string& message)
+{
+    std::cout << message << std::endl;
+}
+
---
+++ clang_gcc/test.cxx
@@ -0,0 +1,10 @@
+#include <string>
+
+void print_message(std::string);
+
+int main()
+{
+    std::string message = "Hello World!";
+    print_message(message);
+    return 0;
+}
