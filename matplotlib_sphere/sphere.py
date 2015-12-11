21f915b3954f7cdae31eda8bcef8006bf2c403b2
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-12-11 09:47:53 +0100

    Plotting sphere

--- matplotlib_sphere/sphere.py
+++ matplotlib_sphere/sphere.py
@@ -1,26 +1,65 @@
-import pylab as pl
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+import matplotlib.pyplot as pl
+from mpl_toolkits.basemap import Basemap, shiftgrid, addcyclic
 
-# Create a sphere.
-r = 1.
-phi, theta = np.mgrid[0.0:np.pi:100j, 0.0:2.0*np.pi:100j]
+lon = np.linspace(  0., 360., 360)
+lat = np.linspace(-90.,  90., 180)
 
+# make 2-d grid of lons, lats
+lons, lats = np.meshgrid(lon, lat)
 
-x = (r + 0.2*np.exp( -(phi)**2. / 0.04) ) * np.sin(phi) * np.cos(theta)
-y = (r + 0.2*np.exp( -(phi)**2. / 0.04) ) * np.sin(phi) * np.sin(theta)
-z = (r + 0.2*np.exp( -(phi)**2. / 0.04) ) * np.cos(phi)
+lonloc1 = 0.
+latloc1 = 50.
+lonloc2 = 120.
+latloc2 = 20.
 
-# Set colours and render.
-fig = pl.figure()
-ax = fig.add_subplot(111, projection='3d')
+# angles in radians
+radlons = lons*np.pi/180.
+radlats = lats*np.pi/180.
+radlonloc1 = lonloc1*np.pi/180.
+radlatloc1 = latloc1*np.pi/180.
+radlonloc2 = lonloc2*np.pi/180.
+radlatloc2 = latloc2*np.pi/180.
 
-#ax.plot_surface(x, y, z,  rstride=1, cstride=1, color='c', alpha=1., linewidth=0.)
-ax.plot_surface(x, y, z,  rstride=1, cstride=1, cmap=pl.cm.gist_earth, vmin=1., vmax=1.2, linewidth=0.)
-ax.set_xlim([-1, 1])
-ax.set_ylim([-1, 1])
-ax.set_zlim([-1, 1])
-ax.set_aspect("equal")
-pl.tight_layout()
+r = 6370997.
+radlondiff1 = abs(radlons-radlonloc1)
+radlondiff2 = abs(radlons-radlonloc2)
+d1 = r*np.arccos( np.sin(radlats)*np.sin(radlatloc1) \
+                + np.cos(radlats)*np.cos(radlatloc1) * np.cos(radlondiff1) )
+d2 = r*np.arccos( np.sin(radlats)*np.sin(radlatloc2) \
+                + np.cos(radlats)*np.cos(radlatloc2) * np.cos(radlondiff2) )
+
+dstd = 2.e6
+h = np.exp( -d1**2 / dstd**2) - np.exp( -d2**2 / dstd**2)
+
+# make orthographic basemap.
+m = Basemap(resolution='c',projection='ortho',lat_0=60.,lon_0=100.)
+
+# create figure, add axes
+pl.close('all')
+fig1 = pl.figure(figsize=(8,10))
+ax = fig1.add_axes([0.1,0.1,0.8,0.8])
+
+# compute native x,y coordinates of grid.
+x, y = m(lons, lats)
+
+# define parallels and meridians to draw.
+parallels = np.arange(-80.,90,20.)
+meridians = np.arange(0.,360.,20.)
+
+# plot SLP contours.
+cs = np.linspace(-1., 1., 21)
+c1 = m.contour (x, y, h, cs, linewidths=0.5, colors='k', extend='both')
+c2 = m.contourf(x, y, h, cs, cmap=pl.cm.RdYlBu)
+
+# draw coastlines, parallels, meridians.
+#m.drawcoastlines(linewidth=1.5)
+m.drawparallels(parallels)
+m.drawmeridians(meridians)
+
+# add colorbar
+cb = m.colorbar(c2, "bottom", size="5%", pad="2%")
 pl.show()
 
+#lonidx = np.abs(lon-lonloc).argmin()
+#latidx = np.abs(lat-latloc).argmin()
