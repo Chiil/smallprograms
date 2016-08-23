0f0b0d86e9dfa52c3ab2b70ace80f2c63ac4a4da
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2016-08-23 15:05:10 +0200

    use enum

--- bitshift/bitshift.cxx
+++ bitshift/bitshift.cxx
@@ -5,6 +5,7 @@
 namespace
 {
     const int fieldsize = 12;
+    enum face { c = 0x1, u = 0x2, v = 0x4, w = 0x8 };
 }
 
 int main()
@@ -23,10 +24,10 @@ int main()
     std::cout << "field: " << std::endl;
     for (int i : field)
         std::cout << i << ", "
-                  << ((i & 0x1) >> 0) << ", " 
-                  << ((i & 0x2) >> 1) << ", " 
-                  << ((i & 0x4) >> 2) << ", " 
-                  << ((i & 0x8) >> 3) << ", " 
+                  << ((i & face::c) >> 0) << ", " 
+                  << ((i & face::u) >> 1) << ", " 
+                  << ((i & face::v) >> 2) << ", " 
+                  << ((i & face::w) >> 3) << ", " 
                   << std::endl;
     std::cout << std::endl;
 
@@ -35,10 +36,10 @@ int main()
     for (int i=0; i<fieldsize/4; ++i)
     {
         const int index = 4*i;
-        mask[i] = (field[index  ] & 0x1)
-                + (field[index+1] & 0x2)
-                + (field[index+2] & 0x3)
-                + (field[index+3] & 0x4);
+        mask[i] = (field[index  ] & face::c)
+                + (field[index+1] & face::u)
+                + (field[index+2] & face::v)
+                + (field[index+3] & face::w);
     }
 
     return 0;
