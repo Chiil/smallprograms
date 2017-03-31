7def9a78ea91b7f498e108e053dc8cd8955543c6
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2017-03-31 14:58:59 +0200

    Changed hexagon resolution

--- hexagons/hexagon.py
+++ hexagons/hexagon.py
@@ -2,21 +2,19 @@ import numpy as np
 import pylab as pl
 from scipy import interpolate
 
-nx = 100
+nx = 32
 Lx = 1.
 dx = Lx / nx
 dy = (.75)**.5 * dx
 
-ny = int(Lx / dy)
-Ly = Lx - Lx % dy
-
-print(nx, ny)
+ny = int(Lx/dy)
+Ly = Lx - Lx%dy
 
 points = []
 
 for j in range(ny+1):
     y = j*dy
-    j_odd = j % 2
+    j_odd = j%2
     for i in range(nx+1-j_odd):
         x = (i + j_odd/2)*dx
         points.append((x,y))
@@ -32,14 +30,17 @@ sigma = 0.05
 z = np.exp( - ((x-xm)**2) / (2.*sigma**2) ) \
   * np.exp( - ((y-ym)**2) / (2.*sigma**2) )
 
-x_plot, y_plot = np.meshgrid(np.linspace(0, Lx, 1000), np.linspace(0, Ly, 1000))
-#z_plot = interpolate.griddata(points, z, (x_plot, y_plot), method='nearest')
-z_plot = interpolate.griddata(points, z, (x_plot, y_plot), method='cubic')
+#z = np.random.rand(len(z))
+
+x_plot, y_plot = np.meshgrid(np.linspace(0, Lx, 2000), np.linspace(0, Ly, 2000))
+z_plot = interpolate.griddata(points, z, (x_plot, y_plot), method='nearest')
+#z_plot = interpolate.griddata(points, z, (x_plot, y_plot), method='cubic')
+
 
 pl.figure()
 pl.subplot(111, aspect='equal')
 pl.plot(x, y, 'k.', markersize=0.5)
-pl.pcolormesh(x_plot, y_plot, z_plot, vmin=0., vmax=1.)
+pl.pcolormesh(x_plot, y_plot, z_plot, vmin=0., vmax=1., cmap=pl.cm.jet)
 pl.colorbar()
 pl.xlim(0, Lx)
 pl.ylim(0, Ly)
