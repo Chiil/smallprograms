cbd42b097821f9c361eb8c3c86ba54ebb5c42db3
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2017-06-27 15:34:20 +0200

    Use a pass by value for the number, and convert to size_t

--- cpp_fortran/main.cxx
+++ cpp_fortran/main.cxx
@@ -3,15 +3,14 @@
 
 namespace
 {
-    extern "C" void square(double*, int*);
+    extern "C" void square(double*, size_t);
 }
 
 int main()
 {
     std::vector<double> a = {1., 2., 3.};
 
-    int size = a.size();
-    square(a.data(), &size);
+    square(a.data(), a.size());
 
     for (const double d : a)
         std::cout << d << "\n";
--- cpp_fortran/square.f90
+++ cpp_fortran/square.f90
@@ -1,8 +1,8 @@
 subroutine square(a, n) bind(c, name="square")
-    use, intrinsic :: iso_c_binding, only : c_double, c_int
+    use, intrinsic :: iso_c_binding, only : c_double, c_size_t
     implicit none
     real(c_double), intent(inout), dimension(n) :: a
-    integer(c_int), intent(in) :: n
+    integer(c_size_t), value :: n
 
     a(1:n) = a(1:n)**2
 end subroutine
