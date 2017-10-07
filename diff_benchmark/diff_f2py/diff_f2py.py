bb63e2c6b9cf5efac6d63a17464ed8e5104da656
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2017-10-07 23:44:05 +0200

    Working example variable precision fortran

--- diff_benchmark/diff_f2py/diff_f2py.py
+++ diff_benchmark/diff_f2py/diff_f2py.py
@@ -1,6 +1,13 @@
 import numpy as np
-import diff
 from timeit import default_timer as timer
+
+float_type = np.float32
+#float_type = np.float64
+
+if (float_type == np.float32):
+    import diff_f as diff
+elif (float_type == np.float64):
+    import diff_d as diff
 
 nloop = 100;
 itot = 384;
@@ -8,10 +15,11 @@ jtot = 384;
 ktot = 384;
 ncells = itot*jtot*ktot;
 
-at = np.zeros((ktot, jtot, itot))
+at = np.zeros((ktot, jtot, itot), dtype=float_type)
 
-index = np.arange(ncells)
+index = np.arange(ncells, dtype=float_type)
 a = (index/(index+1))**2
+del(index)
 a.shape = (ktot, jtot, itot)
 
 # Check results
@@ -21,7 +29,7 @@ print("at={0}".format(at.flatten()[itot*jtot+itot+itot//2]))
 # Time the loop
 start = timer()
 for i in range(nloop):
-    diff.diff(at.T, a.T, 0.1, 0.1, 0.1, 0.1, itot, jtot, ktot)
+    diff.diff(at.T, a.T, 0.1, 0.1, 0.1, 0.1)
 end = timer()
 
 print("Time/iter: {0} s ({1} iters)".format((end-start)/nloop, nloop))
--- diff_benchmark/diff_f2py/diff_f90.f90
+++ diff_benchmark/diff_f2py/diff_f90.f90
@@ -1,21 +1,22 @@
 module diff
 contains
     subroutine diff_f90(at, a, visc, dxidxi, dyidyi, dzidzi, itot, jtot, ktot)
-        double precision, intent(inout), dimension(itot, jtot, ktot) :: at
-        double precision, intent(in), dimension(itot, jtot, ktot) :: a
-        double precision, intent(in) :: visc, dxidxi, dyidyi, dzidzi
+        use, intrinsic :: iso_c_binding, only : TF
+        real(TF), intent(inout), dimension(itot, jtot, ktot) :: at
+        real(TF), intent(in), dimension(itot, jtot, ktot) :: a
+        real(TF), intent(in) :: visc, dxidxi, dyidyi, dzidzi
         integer, intent(in) :: itot, jtot, ktot
 
         do k = 2, ktot-1
             do j = 2, jtot-1
                 do i = 2, itot-1
                     at(i,j,k) = at(i,j,k) + visc * ( &
-                        + ( (a(i+1, j  , k  ) - a(i  , j  , k  )) &
-                          - (a(i  , j  , k  ) - a(i-1, j  , k  )) ) * dxidxi &
-                        + ( (a(i  , j+1, k  ) - a(i  , j  , k  )) &
-                          - (a(i  , j  , k  ) - a(i  , j-1, k  )) ) * dyidyi &
-                        + ( (a(i  , j  , k+1) - a(i  , j  , k  )) &
-                          - (a(i  , j  , k  ) - a(i  , j  , k-1)) ) * dzidzi &
+                        + ( (a(i+1, j  , k  ) - a(i  , j  , k  ) ) &
+                          - (a(i  , j  , k  ) - a(i-1, j  , k  ) ) ) * dxidxi &
+                        + ( (a(i  , j+1, k  ) - a(i  , j  , k  ) ) &
+                          - (a(i  , j  , k  ) - a(i  , j-1, k  ) ) ) * dyidyi &
+                        + ( (a(i  , j  , k+1) - a(i  , j  , k  ) ) &
+                          - (a(i  , j  , k  ) - a(i  , j  , k-1) ) ) * dzidzi &
                         )
                 end do
             end do
