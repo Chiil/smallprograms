b17c2553bdc0872b3cf4930d48ef9481e7f7318d
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2017-10-08 10:10:50 +0200

    Added a different variant of Cython

---
+++ diff_benchmark/diff_cython_2/diff_cython.py
@@ -0,0 +1,33 @@
+import numpy as np
+from timeit import default_timer as timer
+import diff
+
+float_type = np.float32
+#float_type = np.float64
+
+
+nloop = 100;
+itot = 384;
+jtot = 384;
+ktot = 384;
+ncells = itot*jtot*ktot;
+
+at = np.zeros((ktot, jtot, itot), dtype=float_type)
+
+index = np.arange(ncells, dtype=float_type)
+a = (index/(index+1))**2
+del(index)
+a.shape = (ktot, jtot, itot)
+
+# Check results
+diff.diff(at, a, 0.1, 0.1, 0.1, 0.1)
+print("at={0}".format(at.flatten()[itot*jtot+itot+itot//2]))
+
+# Time the loop
+start = timer()
+for i in range(nloop):
+    diff.diff(at, a, 0.1, 0.1, 0.1, 0.1)
+end = timer()
+
+print("Time/iter: {0} s ({1} iters)".format((end-start)/nloop, nloop))
+
---
+++ diff_benchmark/diff_cython_2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+import numpy
+
+setup(
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [Extension("diff",
+                             sources=["diff.pyx"],
+                             language="c++",
+                             extra_compile_args=["-Ofast", "-march=native", "-mtune=native", "-fno-wrapv"],
+                             include_dirs=[numpy.get_include()])],
+)
