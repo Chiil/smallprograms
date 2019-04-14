1e39b1a66e3ede1d098077601f7f416e3e636814
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2019-04-14 14:04:06 +0200

    Even simpler, read the string.

--- datetime_cpp/datetime.cpp
+++ datetime_cpp/datetime.cpp
@@ -32,32 +32,22 @@ double calc_hour_of_day(const std::tm& tm_start, const double time)
 std::string make_string(const std::tm& tm)
 {
     std::stringstream ss;
-    ss << std::setfill('0') << std::setw(4) << tm.tm_year  << "-";
-    ss << std::setfill('0') << std::setw(2) << tm.tm_mon+1 << "-"; // Add one to let Jan == 1.
-    ss << std::setfill('0') << std::setw(2) << tm.tm_mday  << " ";
-    ss << std::setfill('0') << std::setw(2) << tm.tm_hour  << ":";
-    ss << std::setfill('0') << std::setw(2) << tm.tm_min   << ":";
+    // Year is relative to 1900, month count starts at 0.
+    ss << std::setfill('0') << std::setw(4) << tm.tm_year+1900 << "-";
+    ss << std::setfill('0') << std::setw(2) << tm.tm_mon+1     << "-";
+    ss << std::setfill('0') << std::setw(2) << tm.tm_mday      << " ";
+    ss << std::setfill('0') << std::setw(2) << tm.tm_hour      << ":";
+    ss << std::setfill('0') << std::setw(2) << tm.tm_min       << ":";
     ss << std::setfill('0') << std::setw(2) << tm.tm_sec;
     return ss.str();
 }
 
 int main()
 {
-    int year = 3000;
-    int month = 12;
-    int day = 31;
-    int hour = 23;
-    int minute = 30;
-    int second = 0;
+    std::string datetime_string("1982-04-26 10:30:11");
 
     std::tm tm_start{};
-    tm_start.tm_year = year;
-    tm_start.tm_mon  = month-1; // Months since Jan.
-    tm_start.tm_mday = day;
-    tm_start.tm_hour = hour;
-    tm_start.tm_min  = minute;
-    tm_start.tm_sec  = second;
-    std::mktime(&tm_start);
+    strptime(datetime_string.c_str(), "%Y-%m-%d %H:%M:%S", &tm_start);
 
     double time = 0.;
     std::cout << "Datetime: " << make_string(calc_tm_actual(tm_start, time)) << std::endl;
