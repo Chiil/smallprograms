9f12924eba6ef93beafcd99afaeb51a164c986ed
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2019-09-10 11:34:55 +0200

    Added precision switch.

--- nn_benchmark/nn_cython.py
+++ nn_benchmark/nn_cython.py
@@ -1,19 +1,24 @@
 import numpy as np
 from timeit import default_timer as timer
-import nn 
+import nn
+
+float_type = np.float64
 
 nloop = 100;
 itot = 384;
 jtot = 384;
 ktot = 384;
 
-ut = np.zeros((ktot, jtot, itot))
-vt = np.zeros((ktot, jtot, itot))
-wt = np.zeros((ktot, jtot, itot))
+ut = np.zeros((ktot, jtot, itot), dtype=float_type)
+vt = np.zeros((ktot, jtot, itot), dtype=float_type)
+wt = np.zeros((ktot, jtot, itot), dtype=float_type)
 
-u = np.random.random_sample((ktot, jtot, itot))
-v = np.random.random_sample((ktot, jtot, itot))
-w = np.random.random_sample((ktot, jtot, itot))
+u = np.empty((ktot, jtot, itot), dtype=float_type)
+v = np.empty((ktot, jtot, itot), dtype=float_type)
+w = np.empty((ktot, jtot, itot), dtype=float_type)
+u[:,:,:] = np.random.random_sample((ktot, jtot, itot))
+v[:,:,:] = np.random.random_sample((ktot, jtot, itot))
+w[:,:,:] = np.random.random_sample((ktot, jtot, itot))
 
 # Time the loop
 start = timer()
