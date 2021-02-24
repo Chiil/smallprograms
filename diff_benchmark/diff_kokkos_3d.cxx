dfd7b61aa34eab8ac9a15d7c414319b05bd8d574
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-02-24 19:20:26 +0100

    cleanup, remove non-used code

--- diff_benchmark/diff_kokkos_3d.cxx
+++ diff_benchmark/diff_kokkos_3d.cxx
@@ -6,7 +6,6 @@
 
 #include <Kokkos_core.hpp>
 
-
 namespace
 {
     using Array_3d = Kokkos::View<double***, Kokkos::LayoutRight>;
@@ -23,7 +22,7 @@ namespace
             at[i] = 0.;
         }
     }
-    
+
     struct diff
     {
         Array_3d at;
@@ -32,13 +31,13 @@ namespace
         const double dxidxi;
         const double dyidyi;
         const double dzidzi;
-    
+
         diff(
                 Array_3d at_, const Array_3d a_,
                 const double visc_,
                 const double dxidxi_, const double dyidyi_, const double dzidzi_) :
             at(at_), a(a_), visc(visc_), dxidxi(dxidxi_), dyidyi(dyidyi_), dzidzi(dzidzi_) {};
-    
+
         KOKKOS_INLINE_FUNCTION
         void operator()(Array_3d::size_type k, Array_3d::size_type j, Array_3d::size_type i) const
         {
@@ -52,33 +51,6 @@ namespace
                     );
         }
     };
-    
-    void diff_ref(
-            double* const __restrict__ at, const double* const __restrict__ a,
-            const double visc,
-            const double dxidxi, const double dyidyi, const double dzidzi, 
-            const int itot, const int jtot, const int ktot)
-    {
-        const int ii = 1;
-        const int jj = itot;
-        const int kk = itot*jtot;
-    
-        for (int k=1; k<ktot-1; k++)
-            for (int j=1; j<jtot-1; j++)
-                #pragma GCC ivdep
-                for (int i=1; i<itot-1; i++)
-                {
-                    const int ijk = i + j*jj + k*kk;
-                    at[ijk] += visc * (
-                            + ( (a[ijk+ii] - a[ijk   ]) 
-                              - (a[ijk   ] - a[ijk-ii]) ) * dxidxi 
-                            + ( (a[ijk+jj] - a[ijk   ]) 
-                              - (a[ijk   ] - a[ijk-jj]) ) * dyidyi
-                            + ( (a[ijk+kk] - a[ijk   ]) 
-                              - (a[ijk   ] - a[ijk-kk]) ) * dzidzi
-                            );
-                }
-    }
 }
 
 int main(int argc, char* argv[])
@@ -105,13 +77,7 @@ int main(int argc, char* argv[])
         Array_3d a ("a" , ktot, jtot, itot);
         Array_3d at("at", ktot, jtot, itot);
 
-        Array_3d_inner at_inner = Kokkos::subview(
-                at,
-                Kokkos::pair<size_type, size_type>(1, ktot-2),
-                Kokkos::pair<size_type, size_type>(1, jtot-2),
-                Kokkos::pair<size_type, size_type>(1, itot-2));
-
-        Range_3d range_3d({1, 1, 1}, {ktot-2, jtot-2, itot-2});
+        Range_3d range_3d({1, 1, 1}, {ktot-1, jtot-1, itot-1}, {8, 0, 0});
 
         init(a.data(), at.data(), ncells);
 
@@ -121,26 +87,21 @@ int main(int argc, char* argv[])
         printf("at=%.20f\n", at.data()[itot*jtot+itot+itot/2]);
 
         // Time performance.
-        std::clock_t start = std::clock(); 
-   
+        std::clock_t start = std::clock();
+
         for (int i=0; i<nloop; ++i)
         {
             Kokkos::parallel_for(range_3d, diff(at, a, visc, dxidxi, dyidyi, dzidzi));
-
-            // diff_ref(
-            //         at.data(), a.data(),
-            //         dxidxi, dyidyi, dzidzi, visc,
-            //         itot, jtot, ktot);
         }
 
         Kokkos::fence();
-  
+
         double duration = (std::clock() - start ) / (double)CLOCKS_PER_SEC;
-   
+
         printf("time/iter = %f s (%i iters)\n", duration/(double)nloop, nloop);
     }
 
     Kokkos::finalize();
-    
+
     return 0;
 }
