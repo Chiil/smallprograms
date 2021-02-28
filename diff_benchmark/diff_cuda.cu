1ef64c8f0a0bcd441975462a2f112b2603ce42f7
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-02-28 15:31:34 +0100

    Cuda version of diff benchmark.

---
+++ diff_benchmark/diff_cuda.cu
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <stdlib.h>
+#include <cstdio>
+#include <ctime>
+#include "math.h"
+
+void init(double* const __restrict__ a, double* const __restrict__ at, const int ncells)
+{
+    for (int i=0; i<ncells; ++i)
+    {
+        a[i]  = pow(i,2)/pow(i+1,2);
+        at[i] = 0.;
+    }
+}
+
+__global__ void diff(
+        double* const __restrict__ at, const double* const __restrict__ a,
+        const double visc, const double dxidxi, const double dyidyi, const double dzidzi, 
+        const int itot, const int jtot, const int ktot)
+{
+    const int i  = blockIdx.x*blockDim.x + threadIdx.x;
+    const int j  = blockIdx.y*blockDim.y + threadIdx.y;
+    const int k  = blockIdx.z;
+
+    const int ii = 1;
+    const int jj = itot;
+    const int kk = itot*jtot;
+
+    const int ijk = i + j*jj + k*kk;
+    at[ijk] += visc * (
+            + ( (a[ijk+ii] - a[ijk   ]) 
+              - (a[ijk   ] - a[ijk-ii]) ) * dxidxi 
+            + ( (a[ijk+jj] - a[ijk   ]) 
+              - (a[ijk   ] - a[ijk-jj]) ) * dyidyi
+            + ( (a[ijk+kk] - a[ijk   ]) 
+              - (a[ijk   ] - a[ijk-kk]) ) * dzidzi
+            );
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Add the grid size as an argument!" << std::endl;
+        return 1;
+    }
+
+    const int nloop = 20;
+    const int itot = std::stoi(argv[1]);
+    const int jtot = std::stoi(argv[1]);
+    const int ktot = std::stoi(argv[1]);
+    const int ncells = itot*jtot*ktot;
+
+    double *a  = new double[ncells];
+    double *at = new double[ncells];
+   
+    init(a, at, ncells);
+
+    double *a_cuda;
+    double *at_cuda;
+
+    cudaMalloc(&a_cuda, ncells*sizeof(double));
+    cudaMalloc(&at_cuda, ncells*sizeof(double));
+
+    cudaMemcpy(a_cuda, a, ncells*sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(at_cuda, at, ncells*sizeof(double), cudaMemcpyHostToDevice);
+
+    const int blocki = 32;
+    const int blockj = 32;
+    const int gridi = itot/blocki + (itot%blocki > 0);
+    const int gridj = jtot/blockj + (jtot%blockj > 0);
+
+    dim3 grid_gpu(gridi, gridj, ktot);
+    dim3 block_gpu(blocki, blockj, 1);
+
+    // Check results
+    diff<<<grid_gpu, block_gpu>>>(
+            at_cuda, a_cuda,
+            0.1, 0.1, 0.1, 0.1,
+            itot, jtot, ktot);
+ 
+    cudaMemcpy(at_cuda, at, ncells*sizeof(double), cudaMemcpyDeviceToHost);
+
+    printf("at=%.20f\n",at[itot*jtot+itot+itot/2]);
+ 
+    // Time performance 
+    std::clock_t start = std::clock(); 
+   
+    for (int i=0; i<nloop; ++i)
+        diff<<<grid_gpu, block_gpu>>>(
+                at_cuda, a_cuda,
+                0.1, 0.1, 0.1, 0.1,
+                itot, jtot, ktot);
+  
+    double duration = (std::clock() - start ) / (double)CLOCKS_PER_SEC;
+   
+    printf("time/iter = %f s (%i iters)\n",duration/(double)nloop, nloop);
+    
+    return 0;
+}
