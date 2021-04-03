2ca83ff378a0a2ebac631d20fce92ccffe1d05b5
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-04-03 11:44:25 +0200

    Exposing array, but data is still copied.

--- pybind11_dict/test.py
+++ pybind11_dict/test.py
@@ -1,5 +1,13 @@
 import test_dict
 
-input_dict = { 'itot' : 256, 'ktot' : 128 }
+input_dict = { 'itot' : 4, 'ktot' : 2}
 output_dict = test_dict.read_dict(input_dict)
-print(output_dict)
+
+field = output_dict['field']
+print(field)
+
+field[:, :] = 2.*field[:, :]
+print(field)
+
+test_dict.print_data()
+
--- pybind11_dict/test_dict.cpp
+++ pybind11_dict/test_dict.cpp
@@ -4,6 +4,29 @@
 
 namespace py = pybind11;
 
+class Array
+{
+    public:
+        Array(const int itot, const int ktot) : v_(itot*ktot)
+        {
+            std::cout << "Constructing Array..." << std::endl;
+            for (int n=0; n<itot*ktot; ++n)
+                v_[n] = n;
+        }
+
+        ~Array()
+        { std::cout << "Destructing Array..." << std::endl; }
+
+        double* data() { return v_.data(); }
+
+        std::vector<double>& v() { return v_; }
+
+    private:
+        std::vector<double> v_;
+};
+
+std::unique_ptr<Array> field;
+
 py::dict read_dict(const py::dict input_dict)
 {
     int itot;
@@ -12,19 +35,27 @@ py::dict read_dict(const py::dict input_dict)
     itot = input_dict["itot"].cast<int>();
     ktot = input_dict["ktot"].cast<int>();
 
-    std::vector<double> v(itot*ktot);
-    for (int i=0; i<itot*ktot; ++i)
-        v[i] = i;
+    field = std::make_unique<Array>(itot, ktot);
 
     py::dict output_dict;
-    output_dict["heating_rate"] = py::array({ktot, itot}, v.data());
+    output_dict["field"] = py::array({ktot, itot}, field->data());
 
     return output_dict;
+}
+
+void print_data()
+{
+    for (double d : field->v())
+        std::cout << d << ", ";
+    std::cout << std::endl;
 }
 
 PYBIND11_MODULE(test_dict, m)
 {
     m.doc() = "Creating an output dictionary based on an input dictionary.";
-    m.def("read_dict", &read_dict, py::return_value_policy::move, "Read a dictionary in C++");
+
+    m.def("read_dict", &read_dict, py::return_value_policy::reference_internal, "Read a dictionary in C++ and return numpy output");
+    m.def("print_data", &print_data, "Print the output array on the C++ side");
+
     m.attr("__version__") = "v0.0.1";
 }
