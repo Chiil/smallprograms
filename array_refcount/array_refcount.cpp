21de4493f297f6817eac7093b17a95a704cb9348
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-05-21 20:46:24 +0200

    Added array class with refcounting and no implicit deep copies.

---
+++ array_refcount/array_refcount.cpp
@@ -0,0 +1,62 @@
+#include <memory>
+#include <vector>
+#include <stdexcept>
+#include <iostream>
+
+
+template<typename T>
+class Array
+{
+    public:
+        Array() { vec = std::make_shared<std::vector<T>>(); }
+        Array(const Array<T>& a) = default;
+        Array(Array<T>&& a) = default;
+        Array<T>& operator=(const Array<T>& a) = default;
+        Array<T>& operator=(Array<T>&& a) = default;
+
+        T* data() const { return vec->data(); }
+        T& operator[](const int idx) const { return vec->at(idx); }
+
+        void resize(const int size)
+        {
+            if (vec->size() == 0)
+                vec->resize(size);
+            else
+                throw std::runtime_error("Cannot resize non-zero array");
+        }
+
+        Array<T> copy() const
+        {
+            Array<T> a_copy;
+            *a_copy.vec = *this->vec;
+            return a_copy;
+        }
+
+    private:
+        std::shared_ptr<std::vector<T>> vec;
+};
+
+template<typename T>
+class Array_1d<T> : public Array<T>
+{
+    Array_1d(const size_t size) : Array<T>(
+}
+
+int main()
+{
+    Array<double> a;
+    a.resize(10);
+    a[3] = 666;
+
+    Array<double> b(a);
+    std::cout << a[3] << ", " << b[3] << std::endl;
+
+    Array<double> c = a.copy();
+    Array<double> d = c;
+
+    c[3] = 777;
+    std::cout << a[3] << ", " << b[3] << ", " << c[3] <<  ", " << d[3] << std::endl;
+
+    return 0;
+}
+
