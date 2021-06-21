39899698446aa5fa38ada10b4ab8aaf82f4defa1
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-06-21 13:03:31 +0200

    Compile time tuner works.

--- kernel_tuner/test_tuner_simpler.cpp
+++ kernel_tuner/test_tuner_simpler.cpp
@@ -3,48 +3,54 @@
 #include <chrono>
 
 
-struct Empty_settings {};
+struct Diff
+{
+    template<int k_block>
+    static void impl(
+            double* const __restrict__ at, const double* const __restrict__ a, const double visc,
+            const double dxidxi, const double dyidyi, const double dzidzi,
+            const int itot, const int jtot, const int ktot)
+    {
+        const int ii = 1;
+        const int jj = itot;
+        const int kk = itot*jtot;
+    
+        #pragma omp parallel for simd
+        for (int kb=1; kb<ktot-1; kb+=k_block)
+            for (int k=kb; k<std::min(ktot-1, kb+k_block); ++k)
+                for (int j=1; j<jtot-1; ++j)
+                    #pragma GCC ivdep
+                    for (int i=1; i<itot-1; ++i)
+                    {
+                        const int ijk = i + j*jj + k*kk;
+                        at[ijk] += visc * (
+                                + ( (a[ijk+ii] - a[ijk   ])
+                                  - (a[ijk   ] - a[ijk-ii]) ) * dxidxi
+                                + ( (a[ijk+jj] - a[ijk   ])
+                                  - (a[ijk   ] - a[ijk-jj]) ) * dyidyi
+                                + ( (a[ijk+kk] - a[ijk   ])
+                                  - (a[ijk   ] - a[ijk-kk]) ) * dzidzi
+                                );
+                    }
+    }
+};
 
-
-template<class Func, class... Args, class Settings = Empty_settings>
-void measure(Func&& f, Args&&... args)
+template<int I, class Func, class... Args>
+void measure(Args... args)
 {
     auto start = std::chrono::high_resolution_clock::now();
     for (int i=0; i<10; ++i)
-        f(std::forward<Args>(args)...);
+        Func::template impl<I>(args...);
     auto end = std::chrono::high_resolution_clock::now();
     double duration = std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
 
-    std::cout << "Duration: " << duration/10 << " (s)" << std::endl;
+    std::cout << "(" << I << ") Duration: " << duration/10 << " (s)" << std::endl;
 }
 
-
-template<int k_block>
-void diff(double* const __restrict__ at, const double* const __restrict__ a, const double visc,
-          const double dxidxi, const double dyidyi, const double dzidzi,
-          const int itot, const int jtot, const int ktot)
+template<class Func, class... Args, int... Is>
+void tune(std::integer_sequence<int, Is...>, Args... args)
 {
-    const int ii = 1;
-    const int jj = itot;
-    const int kk = itot*jtot;
-
-    #pragma omp parallel for simd
-    for (int kb=1; kb<ktot-1; kb+=k_block)
-        for (int k=kb; k<std::min(ktot-1, kb+k_block); ++k)
-            for (int j=1; j<jtot-1; ++j)
-                #pragma GCC ivdep
-                for (int i=1; i<itot-1; ++i)
-                {
-                    const int ijk = i + j*jj + k*kk;
-                    at[ijk] += visc * (
-                            + ( (a[ijk+ii] - a[ijk   ])
-                              - (a[ijk   ] - a[ijk-ii]) ) * dxidxi
-                            + ( (a[ijk+jj] - a[ijk   ])
-                              - (a[ijk   ] - a[ijk-jj]) ) * dyidyi
-                            + ( (a[ijk+kk] - a[ijk   ])
-                              - (a[ijk   ] - a[ijk-kk]) ) * dzidzi
-                            );
-                }
+    (measure<Is, Func, Args...>(args...), ...);
 }
 
 
@@ -62,13 +68,8 @@ int main()
     double dzidzi{1.};
     double visc{1.};
 
-    auto lambda = [&]<int... Ks>(const std::integer_sequence<int, Ks...>)
-    {
-        ((measure(diff<Ks>, at.data(), a.data(), visc, dxidxi, dyidyi, dzidzi, itot, jtot, ktot)), ...);
-    };
-
     constexpr std::integer_sequence<int, 1, 2, 4, 8, 16, 32, 64> k_blocks{};
-    lambda(k_blocks);
+    tune<Diff>(k_blocks, at.data(), a.data(), visc, dxidxi, dyidyi, dzidzi, itot, jtot, ktot);
 
     return 0;
 }
