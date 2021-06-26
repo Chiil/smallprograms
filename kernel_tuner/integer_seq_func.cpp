8caba92e3e1e8ecc35809facf7e08c01cecd24b2
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-06-26 21:06:11 +0200

    Working example.

--- kernel_tuner/integer_seq_func.cpp
+++ kernel_tuner/integer_seq_func.cpp
@@ -15,37 +15,40 @@ struct Print
 struct Print_double
 {
     template<int I, int J, int K>
-    constexpr void exec(const double d)
+    static void exec(const double d)
     {
         std::cout << I << ", " << J << ", " << K << " = " << d << std::endl;
     }
 };
 
 
-template<class Func, int I, int J, int... Ks>
-constexpr void inner(
-        std::integer_sequence<int, Ks...> ks)
+template<class Func, int I, int J, int... Ks, class... Args>
+void inner(
+        std::integer_sequence<int, Ks...> ks,
+        Args... args)
 {
-    (Func::template exec<I, J, Ks>(), ...);
+    (Func::template exec<I, J, Ks>(args...), ...);
 }
 
 
-template<class Func, int I, int... Js, int... Ks>
-constexpr void outer(
+template<class Func, int I, int... Js, int... Ks, class... Args>
+void outer(
         std::integer_sequence<int, Js...> js,
-        std::integer_sequence<int, Ks...> ks)
+        std::integer_sequence<int, Ks...> ks,
+        Args... args)
 {
-    (inner<Func, I, Js>(ks), ...);
+    (inner<Func, I, Js>(ks, args...), ...);
 }
 
 
-template<class Func, int... Is, int... Js, int... Ks>
-constexpr void test(
+template<class Func, int... Is, int... Js, int... Ks, class... Args>
+void test(
         std::integer_sequence<int, Is...> is,
         std::integer_sequence<int, Js...> js,
-        std::integer_sequence<int, Ks...> ks)
+        std::integer_sequence<int, Ks...> ks,
+        Args... args)
 {
-    (outer<Func, Is>(js, ks), ...);
+    (outer<Func, Is>(js, ks, args...), ...);
 }
 
 
@@ -55,8 +58,10 @@ int main()
     constexpr std::integer_sequence<int, 1, 2, 4> js{};
     constexpr std::integer_sequence<int, 32, 64> ks{};
 
+    double d = 3.;
+
     test<Print>(is, js, ks);
-    // test<Print_double>(is, js, ks);
+    test<Print_double>(is, js, ks, d);
 
     return 0;
 }
