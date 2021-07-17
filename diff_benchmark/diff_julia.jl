a7379dbdbf027429095a8d434f64a56d8e5b0cef
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-07-17 23:19:32 +0200

    Add julia diffusion benchmark

---
+++ diff_benchmark/diff_julia.jl
@@ -0,0 +1,39 @@
+using BenchmarkTools
+function diff(
+        at, a,
+        visc, dxidxi, dyidyi, dzidzi,
+        itot, jtot, ktot)
+
+    for k in 2:ktot-1
+        for j in 2:jtot-1
+            for i in 2:itot-1
+                at[i, j, k] += visc * (
+                    (a[i-1, j, k] - 2. * a[i, j, k] + a[i+1, j, k]) * dxidxi +
+                    (a[i, j-1, k] - 2. * a[i, j, k] + a[i, j+1, k]) * dyidyi +
+                    (a[i, j, k-1] - 2. * a[i, j, k] + a[i, j, k+1]) * dzidzi )
+            end
+        end
+    end
+end
+
+itot = 384
+jtot = 384
+ktot = 384
+
+a = Array{Float64, 3}(undef, itot, jtot, ktot)
+at = Array{Float64, 3}(undef, itot, jtot, ktot)
+
+visc = 0.1
+dxidxi = 0.1
+dyidyi = 0.1
+dzidzi = 0.1
+
+@time diff(
+        at, a,
+        visc, dxidxi, dyidyi, dzidzi,
+        itot, jtot, ktot)
+
+@time diff(
+        at, a,
+        visc, dxidxi, dyidyi, dzidzi,
+        itot, jtot, ktot)
