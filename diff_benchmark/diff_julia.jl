0caed6393310cf8b8828684f4d57e880ae27c632
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-07-18 10:08:44 +0200

    Added example with view, but very slow...

--- diff_benchmark/diff_julia.jl
+++ diff_benchmark/diff_julia.jl
@@ -1,4 +1,6 @@
 using BenchmarkTools
+using Einsum
+
 
 function diff(
         at::Array{Float64, 3}, a::Array{Float64, 3},
@@ -17,6 +19,28 @@ function diff(
     end
 end
 
+
+function diff_view(
+        at::Array{Float64, 3}, a::Array{Float64, 3},
+        visc::Float64, dxidxi::Float64, dyidyi::Float64, dzidzi::Float64,
+        itot::Int64, jtot::Int64, ktot::Int64)
+
+    at_c = view(at, 2:itot-1, 2:jtot-1, 2:ktot-1)
+
+    a_c = view(a, 2:itot-1, 2:jtot-1, 2:ktot-1)
+    a_w = view(a, 1:itot-2, 2:jtot-1, 2:ktot-1)
+    a_e = view(a, 3:itot  , 2:jtot-1, 2:ktot-1)
+    a_s = view(a, 2:itot-1, 1:jtot-2, 2:ktot-1)
+    a_n = view(a, 2:itot-1, 3:jtot  , 2:ktot-1)
+    a_b = view(a, 2:itot-1, 2:jtot-1, 1:ktot-2)
+    a_t = view(a, 2:itot-1, 2:jtot-1, 3:ktot  )
+
+    at_c .+= @views visc .* ( (a_w .- 2. .* a_c .+ a_e) .* dxidxi .+
+                              (a_s .- 2. .* a_c .+ a_n) .* dyidyi .+
+                              (a_b .- 2. .* a_c .+ a_n) .* dzidzi )
+end
+
+
 itot = 384
 jtot = 384
 ktot = 384
@@ -30,11 +54,11 @@ dyidyi = 0.1
 dzidzi = 0.1
 
 @btime diff(
-        at, a,
+        $at, $a,
         visc, dxidxi, dyidyi, dzidzi,
         itot, jtot, ktot)
 
-@btime diff(
+@btime diff_view(
         at, a,
         visc, dxidxi, dyidyi, dzidzi,
         itot, jtot, ktot)
