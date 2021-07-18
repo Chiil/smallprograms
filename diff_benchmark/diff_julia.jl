22fc9c53cc416eeaf41f9f5a434510b6cca0b095
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-07-18 13:59:26 +0200

    Added Float32 version.

--- diff_benchmark/diff_julia.jl
+++ diff_benchmark/diff_julia.jl
@@ -2,17 +2,17 @@ using BenchmarkTools
 
 
 function diff(
-        at::Array{Float64, 3}, a::Array{Float64, 3},
-        visc::Float64, dxidxi::Float64, dyidyi::Float64, dzidzi::Float64,
-        itot::Int64, jtot::Int64, ktot::Int64)
+        at, a,
+        visc, dxidxi, dyidyi, dzidzi,
+        itot, jtot, ktot)
 
     for k in 2:ktot-1
         for j in 2:jtot-1
             @simd for i in 2:itot-1
                 @inbounds at[i, j, k] += visc * (
-                    (a[i-1, j  , k  ] - 2. * a[i, j, k] + a[i+1, j  , k  ]) * dxidxi +
-                    (a[i  , j-1, k  ] - 2. * a[i, j, k] + a[i  , j+1, k  ]) * dyidyi +
-                    (a[i  , j  , k-1] - 2. * a[i, j, k] + a[i  , j  , k+1]) * dzidzi )
+                    (a[i-1, j  , k  ] - 2.f0 * a[i, j, k] + a[i+1, j  , k  ]) * dxidxi +
+                    (a[i  , j-1, k  ] - 2.f0 * a[i, j, k] + a[i  , j+1, k  ]) * dyidyi +
+                    (a[i  , j  , k-1] - 2.f0 * a[i, j, k] + a[i  , j  , k+1]) * dzidzi )
             end
         end
     end
@@ -57,7 +57,20 @@ dzidzi = 0.1
         visc, dxidxi, dyidyi, dzidzi,
         itot, jtot, ktot)
 
-@btime diff_view(
-        at, a,
-        visc, dxidxi, dyidyi, dzidzi,
+#@btime diff_view(
+#        at, a,
+#        visc, dxidxi, dyidyi, dzidzi,
+#        itot, jtot, ktot)
+
+a_f = rand(Float32, (itot, jtot, ktot))
+at_f = zeros(Float32, (itot, jtot, ktot))
+
+visc_f = Float32(visc)
+dxidxi_f = Float32(dxidxi)
+dyidyi_f = Float32(dyidyi)
+dzidzi_f = Float32(dzidzi)
+
+@btime diff(
+        at_f, a_f,
+        visc_f, dxidxi_f, dyidyi_f, dzidzi_f,
         itot, jtot, ktot)
