5d93b6d27a8ecdef0764cd1aeceb548ae690aa26
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-07-18 23:28:29 +0200

    Split the julia example in two.

--- diff_benchmark/diff_julia_views.jl
+++ diff_benchmark/diff_julia_views.jl
@@ -2,21 +2,25 @@ using BenchmarkTools
 using LoopVectorization
 
 
-function diff!(
+function diff_view!(
         at, a,
         visc, dxidxi, dyidyi, dzidzi,
         itot, jtot, ktot)
 
-    @tturbo for k in 2:ktot-1
-        for j in 2:jtot-1
-            for i in 2:itot-1
-                at[i, j, k] += visc * (
-                    (a[i-1, j  , k  ] - 2.f0 * a[i, j, k] + a[i+1, j  , k  ]) * dxidxi +
-                    (a[i  , j-1, k  ] - 2.f0 * a[i, j, k] + a[i  , j+1, k  ]) * dyidyi +
-                    (a[i  , j  , k-1] - 2.f0 * a[i, j, k] + a[i  , j  , k+1]) * dzidzi )
-            end
-        end
-    end
+    at_c = view(at, 2:itot-1, 2:jtot-1, 2:ktot-1)
+
+    a_c = view(a, 2:itot-1, 2:jtot-1, 2:ktot-1)
+    a_w = view(a, 1:itot-2, 2:jtot-1, 2:ktot-1)
+    a_e = view(a, 3:itot  , 2:jtot-1, 2:ktot-1)
+    a_s = view(a, 2:itot-1, 1:jtot-2, 2:ktot-1)
+    a_n = view(a, 2:itot-1, 3:jtot  , 2:ktot-1)
+    a_b = view(a, 2:itot-1, 2:jtot-1, 1:ktot-2)
+    a_t = view(a, 2:itot-1, 2:jtot-1, 3:ktot  )
+
+    @tturbo @. at_c += visc * (
+            (a_w - 2. * a_c + a_e) * dxidxi +
+            (a_s - 2. * a_c + a_n) * dyidyi +
+            (a_b - 2. * a_c + a_t) * dzidzi )
 end
 
 
@@ -34,13 +38,13 @@ dxidxi = 0.1
 dyidyi = 0.1
 dzidzi = 0.1
 
-@btime diff!(
+
+# Solve the problem in single precision.
+@btime diff_view!(
         $at, $a,
         $visc, $dxidxi, $dyidyi, $dzidzi,
         $itot, $jtot, $ktot)
 
-
-# Solve the problem in singleprecision.
 a_f = rand(Float32, (itot, jtot, ktot))
 at_f = zeros(Float32, (itot, jtot, ktot))
 
@@ -49,7 +53,7 @@ dxidxi_f = Float32(dxidxi)
 dyidyi_f = Float32(dyidyi)
 dzidzi_f = Float32(dzidzi)
 
-@btime diff!(
+@btime diff_view!(
         $at_f, $a_f,
         $visc_f, $dxidxi_f, $dyidyi_f, $dzidzi_f,
         $itot, $jtot, $ktot)
