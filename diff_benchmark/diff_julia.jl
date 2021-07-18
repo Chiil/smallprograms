e6a9ea77bb504f5033bd21119c3168589d642a5b
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-07-18 12:34:19 +0200

    A bit of cleanup and initialization with random numbers.

--- diff_benchmark/diff_julia.jl
+++ diff_benchmark/diff_julia.jl
@@ -1,5 +1,4 @@
 using BenchmarkTools
-using Einsum
 
 
 function diff(
@@ -11,9 +10,9 @@ function diff(
         for j in 2:jtot-1
             @simd for i in 2:itot-1
                 @inbounds at[i, j, k] += visc * (
-                    (a[i-1, j, k] - 2. * a[i, j, k] + a[i+1, j, k]) * dxidxi +
-                    (a[i, j-1, k] - 2. * a[i, j, k] + a[i, j+1, k]) * dyidyi +
-                    (a[i, j, k-1] - 2. * a[i, j, k] + a[i, j, k+1]) * dzidzi )
+                    (a[i-1, j  , k  ] - 2. * a[i, j, k] + a[i+1, j  , k  ]) * dxidxi +
+                    (a[i  , j-1, k  ] - 2. * a[i, j, k] + a[i  , j+1, k  ]) * dyidyi +
+                    (a[i  , j  , k-1] - 2. * a[i, j, k] + a[i  , j  , k+1]) * dzidzi )
             end
         end
     end
@@ -45,8 +44,8 @@ itot = 384
 jtot = 384
 ktot = 384
 
-a = Array{Float64, 3}(undef, itot, jtot, ktot)
-at = Array{Float64, 3}(undef, itot, jtot, ktot)
+a = rand(Float64, (itot, jtot, ktot))
+at = zeros(Float64, (itot, jtot, ktot))
 
 visc = 0.1
 dxidxi = 0.1
@@ -54,7 +53,7 @@ dyidyi = 0.1
 dzidzi = 0.1
 
 @btime diff(
-        $at, $a,
+        at, a,
         visc, dxidxi, dyidyi, dzidzi,
         itot, jtot, ktot)
 
