446afe4937937e8bcac3dd43a331ff72e7900243
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-07-21 10:20:49 +0200

    Rough idea working, now better processing of expressions needed.

--- diff_benchmark/diff_julia_stencil.jl
+++ diff_benchmark/diff_julia_stencil.jl
@@ -1,16 +1,55 @@
 ## Macros
-macro gradx(arg, offset)
-    if offset == "+" || offset == "0"
-        e = quote
-            ($arg[i+1, j, k] - $arg[i, j, k]) * dxi
-        end
-        esc(e)
-    elseif offset == "-"
-        e = quote
-            ($arg[i, j, k] - $arg[i-1, j, k]) * dxi
-        end
-        esc(e)
+function make_notation(a, arrays, i, j, k)
+    if a in arrays
+        return :( $a[i+$i, j+$j, k+$k] )
+    else
+        return :( $a )
     end
+end
+
+function process_rhs(args, arrays, i, j, k)
+    dump(args)
+    for n in size(args)
+        if typeof(args[n]) == Expr
+            args[n] = process_rhs(args[n].args, arrays, i, j, k)
+        elseif typeof(args[n]) == Symbol
+            args[n] = make_notation(args[n], arrays, i, j, k)
+            println("Symbol: ", args[n])
+        else
+            throw(ArgumentError(args[n], "Dunnowhatodo"))
+        end
+    end
+    dump(args)
+    return args
+end
+
+macro fd(arrays, e)
+    dump(e)
+    i = 0; j = 0; k = 0;
+
+    # Set the left hand side
+    lhs = make_notation(e.args[1], arrays.args, i, j, k)
+
+    # Store the head
+    op = e.head
+
+    # Construct the RHS from the tree
+    rhs_args = copy(e.args[2:end])
+
+    # Recursively work through args
+    rhs = process_rhs(rhs_args, arrays.args, i, j, k)
+
+    eo_stencil = Expr(op, lhs, rhs)
+    eo = quote
+        @tturbo unroll=8 for k in 2:ktot-1
+            for j in 2:jtot-1
+                for i in 2:itot-1
+                    $eo_stencil
+                end
+            end
+        end
+    end
+    print(eo)
 end
 
 ## Packages
@@ -19,17 +58,11 @@ using LoopVectorization
 
 ## Diffusion kernel
 function diff!(
-        at, a,
+        at::AbstractArray{T, 3}, a::AbstractArray{T, 3},
         visc, dxi, dyi, dzi,
-        itot, jtot, ktot)
+        itot, jtot, ktot) where T <: Number
 
-     @tturbo unroll=8 for k in 2:ktot-1
-        for j in 2:jtot-1
-            for i in 2:itot-1
-                at[i, j, k] += visc * ( (@gradx(a, "+") - @gradx(a, "-") ) )
-            end
-        end
-    end
+    @fd (at, a) at += visc * a
 end
 
 ## Set the grid size.
