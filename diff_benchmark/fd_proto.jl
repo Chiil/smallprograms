dbc8797f4a0687c69e4f363d4f96f5488eb970b6
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-07-22 19:54:29 +0200

    Pass the expressions around

--- diff_benchmark/fd_proto.jl
+++ diff_benchmark/fd_proto.jl
@@ -14,18 +14,24 @@ function make_index(a, arrays, i)
     end
 end
 
-function process_expr_args(args, arrays, i)
+function process_expr(ex, arrays, i)
     n = 1
+
+    # if (isa(ex.args[1], Symbol) && ex.args[1] == Symbol("gradx"))
+    #     ex = :( $ex * dxi )
+    # end
+
+    args = ex.args
     while n <= length(args)
         if isa(args[n], Expr)
-            process_expr_args(args[n].args, arrays, i)
+            process_expr(args[n], arrays, i)
             n += 1
         elseif isa(args[n], Symbol)
             if args[n] == Symbol("gradx")
                 if isa(args[n+1], Expr)
                     args[n] = copy(args[n+1])
-                    process_expr_args(args[n  ].args, arrays, i+0.5)
-                    process_expr_args(args[n+1].args, arrays, i-0.5)
+                    process_expr(args[n  ], arrays, i+0.5)
+                    process_expr(args[n+1], arrays, i-0.5)
                 elseif isa(args[n+1], Symbol)
                     args[n] = args[n+1]
                     args[n  ] = make_index(args[n  ], arrays, i+0.5)
@@ -36,8 +42,8 @@ function process_expr_args(args, arrays, i)
             elseif args[n] == Symbol("interpx")
                 if isa(args[n+1], Expr)
                     args[n] = copy(args[n+1])
-                    process_expr_args(args[n  ].args, arrays, i+0.5)
-                    process_expr_args(args[n+1].args, arrays, i-0.5)
+                    process_expr(args[n  ], arrays, i+0.5)
+                    process_expr(args[n+1], arrays, i-0.5)
                 elseif isa(args[n+1], Symbol)
                     args[n] = args[n+1]
                     args[n  ] = make_index(args[n  ], arrays, i+0.5)
@@ -57,9 +63,9 @@ function process_expr_args(args, arrays, i)
 end
 
 macro fd(arrays, ex)
-    process_expr_args(ex.args, arrays.args, 0)
+    process_expr(ex, arrays.args, 0)
     println(ex)
 end
 
 @fd (at, a) at += gradx( interpx( a ) )
-@fd (at, a) at += gradx( gradx( a ) )
+# @fd (at, a) at += gradx( gradx( a ) )
