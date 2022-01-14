83de1259a000e841efadaa5e77297737c62d44dd
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2022-01-14 23:49:26 +0100

    Convert to 3D.

--- julia_mpi/cyclic_bc.jl
+++ julia_mpi/cyclic_bc.jl
@@ -1,6 +1,6 @@
 # User input.
 npx = 3; npy = 3
-itot = 6; jtot = 6
+itot = 6; jtot = 6; ktot = 6
 
 imax = itot ÷ npx; jmax = jtot ÷ npy
 
@@ -16,11 +16,11 @@ mpiid = MPI.Comm_rank(commxy)
 nwest, neast = MPI.Cart_shift(commxy, 1, 1)
 nsouth, nnorth = MPI.Cart_shift(commxy, 0, 1)
 
-data = ones(Int, imax+2, jmax+2) * mpiid
+data = ones(Int, imax+2, jmax+2, ktot+2) * mpiid
 
 # Transfer the east-west data.
-send_east = data[end-1, :]
-send_west = data[2, :]
+send_east = data[end-1, :, :]
+send_west = data[2, :, :]
 recv_west = zeros(Int, size(send_east))
 recv_east = zeros(Int, size(send_west))
 
@@ -31,12 +31,12 @@ reqs[3] = MPI.Isend( send_east, neast, 1, commxy);
 reqs[4] = MPI.Isend( send_west, nwest, 2, commxy);
 MPI.Waitall!(reqs)
 
-data[1, :] .= recv_west[:]
-data[end, :] .= recv_east[:]
+data[1, :, :] .= recv_west[:, :]
+data[end, :, :] .= recv_east[:, :]
 
 # Transfer the north-south data.
 send_north = data[:, end-1, :]
-send_south = data[:, 2]
+send_south = data[:, 2, :]
 recv_south = zeros(Int, size(send_north))
 recv_north = zeros(Int, size(send_south))
 
@@ -46,15 +46,14 @@ reqs[3] = MPI.Isend( send_south, nsouth, 1, commxy);
 reqs[4] = MPI.Isend( send_north, nnorth, 2, commxy);
 MPI.Waitall!(reqs)
 
-data[:, 1] .= recv_south[:]
-data[:, end] .= recv_north[:]
+data[:, 1, :] .= recv_south[:, :]
+data[:, end, :] .= recv_north[:, :]
 
 # Print output.
 if mpiid == 4
     println("Neighbors (W, E): $mpiid = $nwest, $neast")
     println("Neighbors (S, N): $mpiid = $nsouth, $nnorth")
-    println(data[:, 2])
-    println(data[2, :])
+    println(data[:, :, end])
 end
 
 MPI.Barrier(commxy)
