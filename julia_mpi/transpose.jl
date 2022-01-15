8dbf95e3b1caa9b915774d60a5786fff8569e9df
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2022-01-15 11:53:40 +0100

    Add benchmarking to transpose

--- julia_mpi/transpose.jl
+++ julia_mpi/transpose.jl
@@ -7,6 +7,7 @@ imax = itot ÷ npx; jmax = jtot ÷ npy; kmax = ktot ÷ npx
 
 ## Init MPI and create grid.
 using MPI
+using BenchmarkTools
 
 MPI.Init()
 
@@ -18,38 +19,43 @@ commx = MPI.Cart_sub(commxy, [false, true])
 data = ones(Int, imax, jmax, ktot) * mpiid
 data_new = Array{Int}(undef, itot, jmax, kmax)
 
-sendbuf = Array{Int}(undef, imax, jmax, kmax, npx)
-recvbuf = Array{Int}(undef, imax, jmax, kmax, npx)
-
-
-## Load the buffer
-for i in 1:npx
-    ks = (i-1)*kmax + 1
-    ke = i*kmax
-    sendbuf[:, :, :, i] .= data[:, :, ks:ke]
+function transpose_zx(data_new, data)
+    sendbuf = Array{Int}(undef, imax, jmax, kmax, npx)
+    recvbuf = Array{Int}(undef, imax, jmax, kmax, npx)
+    
+    # Load the buffer
+    for i in 1:npx
+        ks = (i-1)*kmax + 1
+        ke = i*kmax
+        sendbuf[:, :, :, i] .= data[:, :, ks:ke]
+    end
+    
+    # Transpose zx
+    reqs = Vector{MPI.Request}(undef, 2*npx)
+    for i in 1:npx
+        tag = 1
+        r = @view recvbuf[:, :, :, i]; s = @view sendbuf[:, :, :, i]
+        reqs[2*(i-1)+1] = MPI.Irecv!(r, i-1, tag, commx)
+        reqs[2*(i-1)+2] = MPI.Isend( s, i-1, tag, commx)
+    end
+    MPI.Waitall!(reqs)
+    # MPI.Alltoall!(MPI.UBuffer(sendbuf, imax*jmax*kmax), MPI.UBuffer(recvbuf, imax*jmax*kmax), commx)
+    
+    # Unload the buffer
+    for i in 1:npx
+        is = (i-1)*imax + 1
+        ie = i*imax
+        data_new[is:ie, :, :] .= recvbuf[:, :, :, i]
+    end
 end
 
-print("s $mpiid: $sendbuf \n")
+transpose_zx(data_new, data)
 
-
-## Transpose zx
-# reqs = Vector{MPI.Request}(undef, 2*npx)
-# for i in 1:npx
-#     tag = 1
-#     reqs[2*(i-1)+1] = MPI.Irecv!(recvbuf[:, :, :, i], i-1, tag, commx)
-#     reqs[2*(i-1)+2] = MPI.Isend( sendbuf[:, :, :, i], i-1, tag, commx)
-# end
-# MPI.Waitall!(reqs)
-MPI.Alltoall!(MPI.UBuffer(sendbuf, imax*jmax*kmax), MPI.UBuffer(recvbuf, imax*jmax*kmax), commx)
-
-print("r $mpiid: $recvbuf \n")
-
-
-## Unload the buffer
-for i in 1:npx
-    is = (i-1)*imax + 1
-    ie = i*imax
-    data_new[is:ie, :, :] .= recvbuf[:, :, :, i]
+for n in 1:10
+    dt = @elapsed transpose_zx(data_new, data)
+    if mpiid == 0 println("Elapsed: $dt (s)") end
 end
 
-# print("d $mpiid: $data_new \n")
+print("$mpiid: $data_new\n")
+
+MPI.Finalize()
