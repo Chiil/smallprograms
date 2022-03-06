40b2b4f54ec20ddc0daaa75aadb1c11fa3573057
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2022-03-06 18:49:28 +0100

    Add advection-diffusion test.

---
+++ julia_zygote/advec_diff_test.jl
@@ -0,0 +1,71 @@
+## Packages.
+using Zygote
+using Flux
+using Flux.Optimise: update!
+using PyPlot
+pygui(true)
+
+
+## Init arrays
+x = 0:1.0:100 |> collect
+sigma = 5.
+y_ref = @. exp( -(x-20)^2 / sigma^2)
+y = copy(y_ref)
+y_ref0 = copy(y_ref)
+
+
+## Function to optimize.
+const visc_ref = 2.
+const u_ref = 1.
+const dt = 0.1
+const dx = x[2] - x[1]
+const dxidxi = 1/(dx^2)
+const dxi2 = 1/(2dx)
+
+function integrate(y, u, visc)
+    yl = @view y[1:end-2]; yc = @view y[2:end-1]; yr = @view y[3:end]
+    advec = - u .* (yr .- yl) .* dxi2
+    diff = visc .* (yl .- 2yc .+ yr) .* dxidxi
+    y_new = yc .+ dt .* (advec .+ diff)
+    return vcat(y_new[1], y_new, y_new[end])
+end
+
+
+## Loss function.
+function loss(y, y_ref)
+    return sum( (  integrate(y    , u_param[1], visc_param[1])
+                .- integrate(y_ref, u_ref     , visc_ref     )).^2 )
+end
+
+
+## Set up optimizer.
+u_param = [0.]
+visc_param = [0.]
+opt = ADAM(0.1)
+θ = params(u_param, visc_param)
+
+
+## Optimize steps.
+# Find the optimal velocity and viscosity.
+for i in 1:1
+    grads = gradient(() -> loss(y, y_ref), θ)
+    for p in (u_param, visc_param,)
+        update!(opt, p, grads[p])
+    end
+end
+
+# Print and plot status.
+println("u_param = $u_param, visc_param = $visc_param, loss = $(loss(y, y_ref))")
+
+# Integrate in time.
+y[:] .= integrate(y, u_param[1], visc_param[1])
+y_ref[:] .= integrate(y_ref, u_ref, visc_ref)
+
+plt.figure()
+plt.subplot(211)
+plt.plot(x, y, "C0-")
+plt.plot(x, y_ref, "k:")
+plt.plot(x, y_ref0, linestyle=":", color="#bbbbbb")
+plt.subplot(212)
+plt.plot(x, y - y_ref, "C1-")
+
