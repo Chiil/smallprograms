b525c6564d742009c73218896d12ce9fbf708abf
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2023-03-14 22:13:41 +0100

    Working example

--- cpp_visit/cpp_visit.cpp
+++ cpp_visit/cpp_visit.cpp
@@ -5,24 +5,33 @@
 struct Advec_disabled {};
 struct Advec_2 {};
 struct Advec_2i5 {};
+using Advec = std::variant<Advec_disabled, Advec_2, Advec_2i5>;
 
 
 template<class... Ts> struct overload : Ts... { using Ts::operator()...; };
 template<class... Ts> overload(Ts...) -> overload<Ts...>; // line not needed in C++20...
 
+void advec_init_disabled() { std::cout << "Disabled\n"; }
+void advec_init_2(const int a) { std::cout << "2\n"; }
+void advec_init_2i5(const int a, const int b) { std::cout << "2i5\n"; }
 
-void advec_init(std::variant<Advec_disabled, Advec_2, Advec_2i5>& advec)
+
+void advec_init(Advec& advec)
 {
     std::visit(overload{
-            []( Advec_disabled& ) { std::cout << "Disabled\n"; },
-            []( Advec_2& ) { std::cout << "2\n"; },
-            []( Advec_2i5& ) { std::cout << "2i5\n"; }}, advec);
+            []( Advec_disabled& ) { advec_init_disabled(); },
+            []( Advec_2& )        { advec_init_2(1); },
+            []( Advec_2i5& )      { advec_init_2i5(5, 7); }
+        }, advec);
 };
 
 
 int main(int argc, char** argv)
 {
-    std::variant<Advec_disabled, Advec_2, Advec_2i5> advec;
+    Advec advec;
+
+    if (argc != 2)
+        throw std::runtime_error("Only 1 argument allowed");
 
     if (std::stoi(argv[1]) == 0)
         advec = Advec_disabled();
@@ -30,6 +39,8 @@ int main(int argc, char** argv)
         advec = Advec_2();
     else if (std::stoi(argv[1]) == 2)
         advec = Advec_2i5();
+    else
+        throw std::runtime_error("Illegal value for advec");
 
     advec_init(advec);
 
