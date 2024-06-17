c41a031eeed4fd0363b36d887e2481cab79acecb
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2024-06-17 22:27:33 +0200

    Add loops back to numba

--- diff_benchmark/diff_numba.py
+++ diff_benchmark/diff_numba.py
@@ -2,8 +2,11 @@ import numpy as np
 from numba import jit, prange
 from timeit import default_timer as timer
 
-@jit(nopython=True, parallel=False, nogil=True)
+@jit(nopython=True, parallel=True, nogil=True)
 def diff(at, a, visc, dxidxi, dyidyi, dzidzi, itot, jtot, ktot):
+    for k in prange(1, ktot-1):
+        for j in prange(1, jtot-1):
+            for i in prange(1, itot-1):
                 at[k, j, i] += visc * ( \
                         + ( (a[k+1, j  , i  ] - a[k  , j  , i  ]) \
                           - (a[k  , j  , i  ] - a[k-1, j  , i  ]) ) * dxidxi \
@@ -29,13 +32,14 @@ del(index)
 a.shape = (ktot, jtot, itot)
 
 # Check results
-diff(at, a, 0.1, 0.1, 0.1, 0.1, itot, jtot, ktot)
+c = float_type(0.1)
+diff(at, a, c, c, c, c, itot, jtot, ktot)
 print("at={0}".format(at.flatten()[itot*jtot+itot+itot//2]))
 
 # Time the loop
 start = timer()
 for i in range(nloop):
-    diff(at, a, 0.1, 0.1, 0.1, 0.1, itot, jtot, ktot)
+    diff(at, a, c, c, c, c, itot, jtot, ktot)
 end = timer()
 
 print("Time/iter: {0} s ({1} iters)".format((end-start)/nloop, nloop))
