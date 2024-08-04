7935d54daf38a17d3378cc1ed7707cba5c88b9cf
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2024-08-04 10:46:50 +0200

    Nuba fix.

--- diff_benchmark/diff_numba.py
+++ diff_benchmark/diff_numba.py
@@ -1,24 +1,28 @@
 import numpy as np
-from numba import jit, prange
+from numba import njit, prange, cuda
 from timeit import default_timer as timer
 
-@jit(nopython=True, parallel=False, nogil=True)
-def diff(at, a, visc, dxidxi, dyidyi, dzidzi, itot, jtot, ktot):
-                at[k, j, i] += visc * ( \
-                        + ( (a[k+1, j  , i  ] - a[k  , j  , i  ]) \
-                          - (a[k  , j  , i  ] - a[k-1, j  , i  ]) ) * dxidxi \
-                        + ( (a[k  , j+1, i  ] - a[k  , j  , i  ]) \
-                          - (a[k  , j  , i  ] - a[k  , j-1, i  ]) ) * dyidyi \
-                        + ( (a[k  , j  , i+1] - a[k  , j  , i  ]) \
-                          - (a[k  , j  , i  ] - a[k  , j  , i-1]) ) * dzidzi )
-
-float_type = np.float32
-# float_type = np.float64
-
-nloop = 30;
 itot = 384;
 jtot = 384;
 ktot = 384;
+
+@njit
+def diff(at, a, visc, dxidxi, dyidyi, dzidzi):
+    for k in prange(1, ktot-1):
+        for j in prange(1, jtot-1):
+            for i in prange(1, itot-1):
+                at[k, j, i] += visc * (
+                        + ( (a[k+1, j  , i  ] - a[k  , j  , i  ])
+                          - (a[k  , j  , i  ] - a[k-1, j  , i  ]) ) * dxidxi
+                        + ( (a[k  , j+1, i  ] - a[k  , j  , i  ])
+                          - (a[k  , j  , i  ] - a[k  , j-1, i  ]) ) * dyidyi
+                        + ( (a[k  , j  , i+1] - a[k  , j  , i  ])
+                          - (a[k  , j  , i  ] - a[k  , j  , i-1]) ) * dzidzi
+                        )
+
+float_type = np.float32
+
+nloop = 30;
 ncells = itot*jtot*ktot;
 
 at = np.zeros((ktot, jtot, itot), dtype=float_type)
@@ -29,13 +33,13 @@ del(index)
 a.shape = (ktot, jtot, itot)
 
 # Check results
-diff(at, a, 0.1, 0.1, 0.1, 0.1, itot, jtot, ktot)
+diff(at, a, 0.1, 0.1, 0.1, 0.1)
 print("at={0}".format(at.flatten()[itot*jtot+itot+itot//2]))
 
 # Time the loop
 start = timer()
 for i in range(nloop):
-    diff(at, a, 0.1, 0.1, 0.1, 0.1, itot, jtot, ktot)
+    diff(at, a, 0.1, 0.1, 0.1, 0.1)
 end = timer()
 
 print("Time/iter: {0} s ({1} iters)".format((end-start)/nloop, nloop))
