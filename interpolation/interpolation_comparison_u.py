f00c795bfae3d0b6d09d4b8dce01780ece99f219
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2024-09-21 10:01:56 +0200

    Generic 1D interpolation, for all odd N's

---
+++ interpolation/interpolation_comparison_u.py
@@ -0,0 +1,63 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from numba import njit
+
+N = 9
+
+@njit
+def int_c(a_new, a, istart, iend):
+    for ic in range(istart, iend):
+        i = (ic-istart)*N + istart + N//2
+
+        a_new[i] = a[ic]
+        for ii in range(1, N//2+1):
+            fac = ii/N
+            a_new[i-ii] = (1-fac)*a[ic] + fac*a[ic-1]
+            a_new[i+ii] = (1-fac)*a[ic] + fac*a[ic+1]
+
+@njit
+def int_u(a_new, a, istart, iend):
+    for ic in range(istart, iend):
+        i = (ic-istart)*N + istart
+        a_new[i] = a[ic]
+        for ii in range(1, N):
+            fac = ii/N
+            a_new[i+ii] = (1-fac)*a[ic] + fac*a[ic+1]
+
+dx = 200
+xsize = 3200
+
+
+## U
+x = np.arange(0, xsize+dx/2, dx)
+u = np.sin(2*(2*np.pi)/xsize * x) + 0.5*np.sin(5*(2*np.pi)/xsize * x)
+
+dx_new = dx / N
+
+x_int = np.arange(0, xsize+dx_new/2, dx_new)
+u_int = np.zeros_like(x_int)
+
+int_u(u_int, u, 0, len(x)-1)
+
+plt.figure()
+plt.plot(x_int, u_int)
+plt.plot(x, u, 'k:')
+
+
+## C
+x = np.arange(-dx/2, xsize+dx, dx)
+s = np.sin(2*(2*np.pi)/xsize * x) + 0.5*np.sin(5*(2*np.pi)/xsize * x)
+
+dx_new = dx / N
+
+x_int = np.arange(-dx_new/2, xsize+dx_new, dx_new)
+s_int = np.zeros_like(x_int)
+
+int_c(s_int, s, 1, len(x)-1)
+
+plt.figure()
+plt.plot(x_int[1:-1], s_int[1:-1])
+plt.plot(x, s, 'k:')
+
+
+plt.show()
