3db4bdd042225e02825efca5a7f33279b487f810
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2024-10-26 19:36:35 +0200

    Add BSpline

--- interpolation/mass_w.py
+++ interpolation/mass_w.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.interpolate import CubicSpline
+import scipy.interpolate as interpolate
 
 
-N = 3
+N = 7
 
 
 def int_lin(a_new, a, istart, iend):
@@ -51,11 +51,11 @@ dx = 100
 xsize = 3200
 
 x = np.arange(-dx/2, xsize+dx, dx)
-w = np.sin(8*(2*np.pi)/xsize * x) + 0.5*np.sin(5*(2*np.pi)/xsize * x)
+w = np.sin(8*(2*np.pi)/xsize * x - xsize/4) + 0.5*np.sin(5*(2*np.pi)/xsize * x - xsize/4)
 
 dx_ref = dx / N
 x_ref = np.arange(-dx_ref/2, xsize+dx_ref, dx_ref)
-w_ref = np.sin(8*(2*np.pi)/xsize * x_ref) + 0.5*np.sin(5*(2*np.pi)/xsize * x_ref)
+w_ref = np.sin(8*(2*np.pi)/xsize * x_ref - xsize/4) + 0.5*np.sin(5*(2*np.pi)/xsize * x_ref - xsize/4)
 
 dx_new = dx / N
 
@@ -81,7 +81,9 @@ for i in range(len(w)-2):
 
 w_spl = np.empty_like(x_int)
 w_spl[:] = np.nan
-spl = CubicSpline(x, w)
+# spl = interpolate.CubicSpline(x, w)
+print(w[:-1][0], w[:-1][-1])
+spl = interpolate.CubicSpline(x[:-1], w[:-1], bc_type='periodic')
 w_spl[:] = spl(x_int)
 w2_spl = w_spl**2
 
@@ -94,19 +96,37 @@ for i in range(len(w)-2):
     w2_spl_mean[istart:iend] = w2_spl_mean[istart:iend].mean()
 
 
-print("ref"     , w[1:-1]    .mean(), (w[1:-1]**2).mean())
-print("nn"      , w_nn[1:-1] .mean(), w2_nn[1:-1] .mean())
-print("lin"     , w_lin[1:-1].mean(), w2_lin[1:-1].mean())
-# print("lin_mean", w_lin[1:-1].mean(), w2_lin_mean[1:-1].mean())
-print("spline  ", w_spl[1:-1].mean(), w2_spl[1:-1].mean())
+w_spl2 = np.empty_like(x_int)
+w_spl2[:] = np.nan
+t, c, k = interpolate.splrep(x, w, s=0, k=5)
+func_spline = interpolate.BSpline(t, c, k, extrapolate=False)
+w_spl2[:] = func_spline(x_int)
+w2_spl2 = w_spl2**2
+
+w_spl2_mean = w_spl2.copy()
+w2_spl2_mean = w2_spl2.copy()
+for i in range(len(w)-2):
+    istart = N*i + 1
+    iend = istart + N
+    w_spl2_mean[istart:iend] = w_spl2_mean[istart:iend].mean()
+    w2_spl2_mean[istart:iend] = w2_spl2_mean[istart:iend].mean()
+
+
+print("ref", w[1:-1]    .mean(), (w[1:-1]**2).mean())
+print("nn", w_nn[1:-1] .mean(), w2_nn[1:-1] .mean())
+print("lin", w_lin[1:-1].mean(), w2_lin[1:-1].mean())
+print("spline cubic", w_spl[1:-1].mean(), w2_spl[1:-1].mean())
+print("spline BSpline", w_spl2[1:-1].mean(), w2_spl2[1:-1].mean())
 
 
 plt.figure()
 plt.plot(x_int[1:-1], w_nn[1:-1], 'C0-', label='nn')
 plt.plot(x_int[1:-1], w_lin[1:-1], 'C1-', label='lin')
-# plt.plot(x_int[1:-1], w_lin_mean[1:-1])
+# plt.plot(x_int[1:-1], w_lin_mean[1:-1], 'C1:', label='lin_mean')
 plt.plot(x_int[1:-1], w_spl[1:-1], 'C3-', label='cubic')
 # plt.plot(x_int[1:-1], w_spl_mean[1:-1], 'C3-', label='cubic')
+plt.plot(x_int[1:-1], w_spl2[1:-1], 'C2-', label='BSpline')
+# plt.plot(x_int[1:-1], w_spl2_mean[1:-1], 'C2:', label='BSpline_mean')
 plt.plot(x[1:-1], w[1:-1], 'ko')
 plt.plot(x_ref[1:-1], w_ref[1:-1], 'k:')
 plt.title('w')
@@ -115,9 +135,11 @@ plt.legend()
 plt.figure()
 plt.plot(x_int[1:-1], w2_nn[1:-1], 'C0-', label='nn')
 plt.plot(x_int[1:-1], w2_lin[1:-1], 'C1-', label='lin')
-# plt.plot(x_int[1:-1], w2_lin_mean[1:-1], 'C1-', label='lin'))
+# plt.plot(x_int[1:-1], w2_lin_mean[1:-1], 'C1:', label='lin_mean')
 plt.plot(x_int[1:-1], w2_spl[1:-1], 'C3-', label='cubic')
 # plt.plot(x_int[1:-1], w2_spl_mean[1:-1], 'C3-', label='cubic')
+plt.plot(x_int[1:-1], w2_spl2[1:-1], 'C2-', label='BSpline')
+# plt.plot(x_int[1:-1], w2_spl2_mean[1:-1], 'C2:', label='BSpline_mean')
 plt.plot(x[1:-1], w[1:-1]**2, 'ko')
 plt.plot(x_ref[1:-1], w_ref[1:-1]**2, 'k:')
 plt.title('w^2')
