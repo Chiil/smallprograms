a9cbd34c8e587881eb244c41549ebe4d40489de7
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2025-02-28 16:46:03 +0100

    Made B and kext spatial.

--- monte_carlo_lw/monte_carlo_lw.py
+++ monte_carlo_lw/monte_carlo_lw.py
@@ -2,57 +2,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Initializing space
-Nphots         = int(5e4)
-x_range        = 10 # from x = 0 to x = 1000
+n_photons = 2**18
+x_range = 10
 
 # Checking flux at these points:
 dn = 0.01
-arr_flux_point = np.arange(0, x_range + dn, dn)
+arr_xh = np.arange(0, x_range+dn/2, dn) # cell edges
+arr_x = np.arange(dn/2, x_range, dn) # cell centers
 
 # Creating photon position and travel distance
-arr_tau = -np.log(np.random.rand(Nphots))
-arr_pos = np.random.rand(Nphots)*x_range
+arr_tau = -np.log(np.random.rand(n_photons))
+arr_pos = np.random.rand(n_photons)*x_range
 
 # RTE properties
 # dI = -kext*I*dn + kext*B*dn
-kext = 1
-B    = 1
+kext = 1.0*np.ones_like(arr_x)
+B = 1.0*np.ones_like(arr_x)
 
 # Creating analytical solution
-arr_I = np.zeros(len(arr_flux_point))
+arr_I = np.zeros_like(arr_xh)
 for i in range(1, len(arr_I)):
-    arr_I[i] = arr_I[i-1] + -kext*arr_I[i-1]*dn + kext*B*dn
+    arr_I[i] = arr_I[i-1] + -kext[i-1]*arr_I[i-1]*dn + kext[i-1]*B[i-1]*dn
 
 # Photon travel distance and next position
-arr_dn       = arr_tau / kext
+arr_dn = arr_tau / kext[0]
 arr_pos_next = arr_pos + arr_dn
 
-
 # Calculating power
-phi_tot = kext * B * (x_range*1*1)
-phi_per_phot = phi_tot/Nphots
+phi_tot = kext[0] * B[0] * (x_range*1*1)
+phi_per_phot = phi_tot/n_photons
 
 # Checking flux at flux points
-arr_FW = np.zeros(len(arr_flux_point))
-for i, flux_point in enumerate(arr_flux_point):
-    arr_going_through_F = (arr_pos < flux_point) & (arr_pos_next > flux_point)
-    FW = np.sum(arr_going_through_F)
-    arr_FW[i] = FW
+arr_flux = np.zeros_like(arr_xh)
+for i, flux_point in enumerate(arr_xh):
+    arr_through_cell_edge = (arr_pos < flux_point) & (arr_pos_next > flux_point)
+    flux = np.sum(arr_through_cell_edge)
+    arr_flux[i] = flux
 
 # Converting MC weights to energy
-arr_F = arr_FW*phi_per_phot
+arr_I_MC = arr_flux*phi_per_phot
     
 
 # Plotting
 plt.figure()
-plt.plot(arr_flux_point, arr_I, color='red', 
-         label=r'dI = -k$\cdot$I$\cdot$B$\cdot$dn + k$\cdot$B$\cdot$dn', 
-         linewidth=3, linestyle='dashed')
-plt.plot(arr_flux_point, arr_F, color='black', label='1D Monte Carlo')
-plt.plot(arr_flux_point, arr_F - arr_I, 'k:', label='1D MC error')
+plt.plot(arr_xh, arr_I, 'r-', label=r'dI = -k$\cdot$I$\cdot$B$\cdot$dn + k$\cdot$B$\cdot$dn')
+plt.plot(arr_xh, arr_I_MC, color='black', label='1D MC')
+plt.plot(arr_xh, arr_I_MC - arr_I, 'k:', label='1D MC error')
 plt.grid(which='major', alpha=0.5)
 plt.grid(which='minor', alpha=0.2)
 plt.minorticks_on()
 plt.legend()
-plt.title('1D Monte Carlo vs Radiation transfer EQ')
+plt.title(f'1D MC longwave with {n_photons} samples')
 plt.show()
