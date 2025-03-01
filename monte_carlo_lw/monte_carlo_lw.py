192415633d221ed4b1d6a87599a6220a5c007dfd
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2025-03-01 10:28:00 +0100

    A few formatting tweaks.

--- monte_carlo_lw/monte_carlo_lw.py
+++ monte_carlo_lw/monte_carlo_lw.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 ## SIMULATION SETTINGS AND GRID GENERATION
 # Initializing space
-n_photons = 2**18
+n_photons = 2**19
 x_range = 10
 
 # Checking flux at these points:
@@ -12,15 +12,11 @@ dn = 0.01
 arr_xh = np.arange(0, x_range+dn/2, dn) # cell edges
 arr_x = np.arange(dn/2, x_range, dn) # cell centers
 
-# Creating photon position and travel distance
-arr_tau = -np.log(np.random.rand(n_photons))
-arr_pos = np.random.rand(n_photons)*x_range
-
 # RTE properties
 # dI = -kext*I*dn + kext*B*dn
 kext = 1.0*np.ones_like(arr_x)
-# B = 1.0*np.ones_like(arr_x)
-B = 1.0*arr_x/x_range
+B = 1.0*np.ones_like(arr_x)
+# B = 1.0*arr_x/x_range
 
 
 ## REFERENCE SOLUTION
@@ -30,6 +26,10 @@ for i in range(1, len(arr_I)):
 
 
 ## MONTE CARLO SOLUTION
+# Creating photon position and travel distance
+arr_tau = -np.log(np.random.rand(n_photons))
+arr_pos = np.random.rand(n_photons)*x_range
+
 # Photon travel distance and next position
 arr_dn = arr_tau / kext[0]
 arr_pos_next = arr_pos + arr_dn
@@ -49,7 +49,7 @@ for i, flux_point in enumerate(arr_xh):
 arr_I_MC = arr_flux*phi_per_phot
     
 
-# Plotting
+## PLOTTING COMPARISON
 plt.figure()
 plt.plot(arr_xh, arr_I, 'r-', label=r'dI = -k$\cdot$I$\cdot$B$\cdot$dn + k$\cdot$B$\cdot$dn')
 plt.plot(arr_xh, arr_I_MC, color='black', label='1D MC')
@@ -58,5 +58,6 @@ plt.grid(which='major', alpha=0.5)
 plt.grid(which='minor', alpha=0.2)
 plt.minorticks_on()
 plt.legend()
-plt.title(f'1D MC longwave with {n_photons} samples')
+plt.title(f'1D MC longwave with {n_photons:,} samples')
 plt.show()
+
