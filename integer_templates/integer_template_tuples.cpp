49c3293202e3206d98980b493ec4339900c8822b
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2025-04-20 23:46:00 +0200

    More checks and tuple generation with an integer sequence.

--- integer_templates/integer_template_tuples.cpp
+++ integer_templates/integer_template_tuples.cpp
@@ -23,15 +23,30 @@ struct Func2
 };
 
 
-constexpr std::array tuples =
+// constexpr std::array tuples =
+// {
+//     std::make_tuple(1, 1, 1), // No refinement
+//     std::make_tuple(2, 2, 2), std::make_tuple(3, 3, 3), // Equal refinement, 3D run
+//     std::make_tuple(2, 2, 1), std::make_tuple(3, 3, 1), // Horizontal refinement, 3D run
+//     std::make_tuple(2, 1, 2), std::make_tuple(3, 1, 3), // Equal refinement, 2D run
+//     std::make_tuple(2, 1, 1), std::make_tuple(3, 1, 1), // Horizontal refinement, 2D run
+//     std::make_tuple(2, 7, 4), // Some weird config
+// };
+
+
+template<int... Is>
+constexpr auto make_tuples(std::integer_sequence<int, Is...>)
 {
-    std::make_tuple(1, 1, 1), // No refinement
-    std::make_tuple(2, 2, 2), std::make_tuple(3, 3, 3), // Equal refinement, 3D run
-    std::make_tuple(2, 2, 1), std::make_tuple(3, 3, 1), // Horizontal nesting, 3D run
-    std::make_tuple(2, 1, 2), std::make_tuple(3, 1, 3), // Equal refinement, 2D run
-    std::make_tuple(2, 1, 1), std::make_tuple(3, 1, 1), // Horizontal nesting, 2D run
-    std::make_tuple(2, 7, 4), // Some weird config
-};
+    return std::array {
+        std::make_tuple(1, 1, 1),       // No refinement (1, 1, 1)
+        std::make_tuple(Is, Is, Is)..., // Equal refinement, 3D run (2, 2, 2)
+        std::make_tuple(Is, Is, 1)...,  // Horizontal refinement, 3D run (2, 2, 1)
+        std::make_tuple(Is, 1, Is)...,  // Equal refinement, 2D run (2, 1, 2)
+        std::make_tuple(Is, 1, 1)...,   // Horizontal refinement, 2D run (2, 1, 1)
+    };
+}
+
+constexpr std::array tuples = make_tuples(std::integer_sequence<int, 2, 3, 4>{});
 
 
 template<class F, int I, int J, int K, class... Args>
@@ -45,8 +60,10 @@ void run_tuples(int i, int j, int k, Args&&... args)
 template<class F, class... Args>
 void call(int i, int j, int k, Args&&... args)
 {
-    if (std::count(tuples.begin(), tuples.end(), std::make_tuple(i, j, k)) != 1)
+    if (std::count(tuples.begin(), tuples.end(), std::make_tuple(i, j, k)) == 0)
         std::cerr << "Chosen refinement ratio (" << i << ", " << j << ", " << k << ") has no compiled function." << std::endl;
+    else if (std::count(tuples.begin(), tuples.end(), std::make_tuple(i, j, k)) > 1)
+        std::cerr << "Chosen refinement ratio (" << i << ", " << j << ", " << k << ") has more than one compiled function." << std::endl;
 
     auto loop_over_tuples = [i, j, k, args...]<std::size_t... Is>(std::index_sequence<Is...>)
     {
@@ -60,9 +77,9 @@ void call(int i, int j, int k, Args&&... args)
 int main()
 {
     call<Func1>(1, 1, 1, 33.0);
+    call<Func1>(2, 2, 2, 33.0);
     call<Func2>(2, 1, 1, 55, 66);
-
-    call<Func1>(1, 2, 4, 12.0);
+    call<Func2>(6, 6, 6, 55, 66);
 
     return 0;
 }
