ecf282a19faea1c8a6c8f405b25de6c0ea68078b
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2025-04-20 16:29:38 +0200

    Allow two functions.

--- integer_templates/integer_template_tuples.cpp
+++ integer_templates/integer_template_tuples.cpp
@@ -3,11 +3,24 @@
 #include <utility>
 
 
-template<int I, int J, int K>
-void func()
+struct Func
 {
-    std::cout << "func<" << I << ", " << J << ", " << K << ">()\n";
-}
+    template<int I, int J, int K>
+    static void run()
+    {
+        std::cout << "func<" << I << ", " << J << ", " << K << ">()\n";
+    }
+};
+
+
+struct Func2
+{
+    template<int I, int J, int K>
+    static void run()
+    {
+        std::cout << "func2<" << I << ", " << J << ", " << K << ">()\n";
+    }
+};
 
 
 constexpr std::array tuples =
@@ -22,29 +35,31 @@ constexpr int max_i = 5;
 using FuncType = void(*)();
 
 
-template<std::size_t N, std::size_t... Is>
+template<class F, std::size_t N, std::size_t... Is>
 constexpr auto make_ijk_array(
         std::array<std::tuple<int, int, int>, N> tuple, std::index_sequence<Is...>)
 {
     return std::array<FuncType, sizeof...(Is)>{
-        &func<std::get<0>(tuples[Is]), std::get<1>(tuples[Is]), std::get<2>(tuples[Is])>... };
+        F::template run<std::get<0>(tuples[Is]), std::get<1>(tuples[Is]), std::get<2>(tuples[Is])>... };
 }
 
 
-template<std::size_t N>
+template<class F, std::size_t N>
 constexpr auto make_array(std::array<std::tuple<int, int, int>, N> tuples)
 {
-    return make_ijk_array(tuples, std::make_index_sequence<N>{});
+    return make_ijk_array<F>(tuples, std::make_index_sequence<N>{});
 }
 
 
-constexpr auto func_table = make_array(tuples);
+template<class F>
+constexpr auto func_table = make_array<F>(tuples);
 
 
+template<class F>
 void call_func(int i, int j, int k)
 {
     if (i >= 0 && i < max_i)
-        func_table[i]();
+        func_table<F>[i]();
     else
         std::cerr << "Invalid combination\n";
 }
@@ -52,8 +67,11 @@ void call_func(int i, int j, int k)
 
 int main()
 {
-    call_func(1, 1, 1);
-    call_func(2, 2, 2);
+    call_func<Func>(1, 1, 1);
+    call_func<Func>(2, 2, 2);
+
+    call_func<Func2>(1, 1, 1);
+    call_func<Func2>(2, 2, 2);
 
     return 0;
 }
