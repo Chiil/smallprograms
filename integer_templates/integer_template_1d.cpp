0e0127d0d0e5412a9898c9a766b059f6e76d3997
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2025-04-20 08:32:10 +0200

    Compile time function arrays.

---
+++ integer_templates/integer_template_1d.cpp
@@ -0,0 +1,42 @@
+#include <array>
+#include <iostream>
+#include <utility>
+
+
+template<int I>
+void func()
+{
+    std::cout << "func<" << I << ">()\n";
+}
+
+
+constexpr int max_i = 5;
+using FuncType = void(*)();
+
+
+// Build 1D array for k
+template<std::size_t... Ks>
+constexpr auto make_array(std::index_sequence<Ks...>) {
+    return std::array<FuncType, sizeof...(Ks)>{ &func<Ks>... };
+}
+
+
+// Generate the full table
+constexpr auto func_table = make_array(std::make_index_sequence<max_i>{});
+
+
+void call_func(int i, int j, int k)
+{
+    if (i >= 0 && i < max_i)
+        func_table[i]();
+    else
+        std::cerr << "Invalid combination\n";
+}
+
+
+int main()
+{
+    call_func(2, 3, 4);
+    call_func(1, 0, 0);
+    return 0;
+}
