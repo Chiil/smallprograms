Correct normalization.

monte_carlo_lw/sampling_trick.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 L = 20.0
 dx = 10.0
-n_samples = 10_000_000
+n_samples = 2**16
 
-k_ext = 1.0
+k_ext = 2.0
 
 dx_bin = 0.01 * dx
 L -= dx_bin # Remove 1 dx for to have the cell centered.
@@ -17,11 +17,11 @@ pos_sample = np.random.uniform(-dx/2, dx/2, size=n_samples)
 tau_sample = -1.0 * np.log(-np.random.uniform(size=n_samples) + 1.0)
 
 s_sample = tau_sample / k_ext
-pos_new_sample = np.where(dir_sample == 0, pos_sample - s_sample, pos_sample + s_sample)
 
 # Reference method.
-hist_ref, _ = np.histogram(pos_new_sample, bins=xh)
-# hist_ref *= dx # Show the fraction of the original cell.
+pos_new = np.where(dir_sample == 0, pos_sample - s_sample, pos_sample + s_sample)
+hist_ref, _ = np.histogram(pos_new, bins=xh)
+hist_ref = hist_ref.astype(np.float64) / n_samples
 
 # Weighting method.
 distance_to_edge = np.where(
@@ -31,8 +31,8 @@ distance_to_edge = np.where(
 )
 weights = np.exp(-k_ext * distance_to_edge)
 pos_new_weights = np.where(dir_sample == 0, -dx/2 - s_sample, dx/2 + s_sample)
-hist_weights, _ = np.histogram(pos_new_weights, bins=xh)#, weights=weights)
-# hist_weights *= dx # Show the fraction of the original cell.
+hist_weights, _ = np.histogram(pos_new_weights, bins=xh, weights=weights)
+hist_weights = hist_weights.astype(np.float64) / n_samples
 
 
 plt.figure()