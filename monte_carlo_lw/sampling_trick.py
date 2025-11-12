1ce4f17fd173f961a8f0a9f349ed3e4ba22641d1
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2025-11-12 15:37:11 +0100

    First step towards weighting method.

--- monte_carlo_lw/sampling_trick.py
+++ monte_carlo_lw/sampling_trick.py
@@ -1,15 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-L = 100.0
-dx = 5.0
-n_samples = 1_000_000
+L = 20.0
+dx = 10.0
+n_samples = 10_000_000
 
-k_ext = 0.25
+k_ext = 1.0
 
-L -= dx # Remove 1 dx for to have the cell centered.
-x = np.arange(dx/2, L, dx) - L/2
-xh = np.arange(0, L+dx/2, dx) - L/2
+dx_bin = 0.01 * dx
+L -= dx_bin # Remove 1 dx for to have the cell centered.
+x = np.arange(dx_bin/2, L, dx_bin) - L/2
+xh = np.arange(0, L+dx_bin/2, dx_bin) - L/2
 
 dir_sample = np.random.randint(2, size=n_samples)
 pos_sample = np.random.uniform(-dx/2, dx/2, size=n_samples)
@@ -18,10 +19,24 @@ tau_sample = -1.0 * np.log(-np.random.uniform(size=n_samples) + 1.0)
 s_sample = tau_sample / k_ext
 pos_new_sample = np.where(dir_sample == 0, pos_sample - s_sample, pos_sample + s_sample)
 
-hist, _ = np.histogram(pos_new_sample, bins=xh, density=True)
-hist *= dx # Show the fraction of the original cell.
+# Reference method.
+hist_ref, _ = np.histogram(pos_new_sample, bins=xh)
+# hist_ref *= dx # Show the fraction of the original cell.
+
+# Weighting method.
+distance_to_edge = np.where(
+        dir_sample == 0,
+        pos_sample + dx/2,
+        dx/2 - pos_sample
+)
+weights = np.exp(-k_ext * distance_to_edge)
+pos_new_weights = np.where(dir_sample == 0, -dx/2 - s_sample, dx/2 + s_sample)
+hist_weights, _ = np.histogram(pos_new_weights, bins=xh)#, weights=weights)
+# hist_weights *= dx # Show the fraction of the original cell.
+
 
 plt.figure()
-plt.plot(x, hist, "C0o-")
+plt.plot(x, hist_ref, "C0+-")
+plt.plot(x, hist_weights, "C1+-")
 plt.show()
 
