0d983d4385a1bcbebdf22327f698a6ad903ce086
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2026-01-13 18:40:03 +0100

    Fixed some bugs, now we need to move the samples.

--- monte_carlo_lw/sampling_trick_3d.py
+++ monte_carlo_lw/sampling_trick_3d.py
@@ -8,8 +8,8 @@ itot = 15
 jtot = 15
 ktot = 15
 
-dx = 10.0
-dy = 10.0
+dx = 40.0
+dy = 20.0
 dz = 10.0
 
 Lx = itot*dx
@@ -32,9 +32,14 @@ zh = np.arange(0, Lz+dz/2, dz) - Lz/2
 
 theta_sample = np.random.uniform(-np.pi, np.pi, size=n_samples)
 phi_sample = np.random.uniform(0.0, 2.0*np.pi, size=n_samples)
-pos_sample = np.random.uniform(-dx/2, dx/2, size=(n_samples, 3))
+pos_sample_x = np.random.uniform(-dx/2, dx/2, size=n_samples)
+pos_sample_y = np.random.uniform(-dy/2, dy/2, size=n_samples)
+pos_sample_z = np.random.uniform(-dz/2, dz/2, size=n_samples)
+pos_sample = np.array([pos_sample_z, pos_sample_y, pos_sample_x]).T
 tau_sample = -1.0 * np.log(-np.random.uniform(size=n_samples) + 1.0)
 dn_sample = tau_sample / k_ext
+
+print(pos_sample.shape)
 
 @njit
 def count_abs(pos):
@@ -42,11 +47,11 @@ def count_abs(pos):
     
     for i in range(n_samples):
         k = int((pos_sample[i, 0] + Lz/2) / dz) 
-        j = int((pos_sample[i, 1] + Ly/2) / dz)
-        i = int((pos_sample[i, 2] + Lx/2) / dy)
+        j = int((pos_sample[i, 1] + Ly/2) / dy)
+        i = int((pos_sample[i, 2] + Lx/2) / dx)
         abs_sample[k, j, i] += 1
 
-    return abs_sample
+    return abs_sample / n_samples
 
 # Reference method.
 abs_ref = count_abs(pos_sample)
@@ -57,9 +62,9 @@ plt.subplot(131)
 plt.pcolormesh(xh, yh, abs_ref[ktot//2, :, :], cmap=cmaps.WhiteBlueGreenYellowRed)
 plt.gca().add_patch(Rectangle((-dx/2, -dy/2), dx, dy, linestyle=':', fill=False))
 plt.subplot(132)
-plt.pcolormesh(xh, zh, abs_ref[:, jtot//2, :])
+plt.pcolormesh(xh, zh, abs_ref[:, jtot//2, :], cmap=cmaps.WhiteBlueGreenYellowRed)
 plt.gca().add_patch(Rectangle((-dx/2, -dz/2), dx, dz, linestyle=':', fill=False))
 plt.subplot(133)
-plt.pcolormesh(yh, zh, abs_ref[:, :, itot//2])
+plt.pcolormesh(yh, zh, abs_ref[:, :, itot//2], cmap=cmaps.WhiteBlueGreenYellowRed)
 plt.gca().add_patch(Rectangle((-dy/2, -dz/2), dy, dz, linestyle=':', fill=False))
 plt.show()
